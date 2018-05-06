a6bbbf4b8c9e21f87168acaa0c76e30958c6c3bc
curro <curro@colegiovivas.com>
2018-05-06 08:56:02 +0200

    bufff

--- Hola.java
+++ Hola.java
@@ -1,7 +1,7 @@
 public class Hola { 
 	int boo;
 	public static void main (String args[]){ 
-		System.out.println("Welcome to de C#"); 
+		System.out.println("Bienvenido to de C#"); 
         int a=0b11101; 
 		int b =234;
 	System.out.println(b);	
