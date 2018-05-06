9cf6b78149a7b3740d5663263364d2e22fe653ef
ColegioVivasCurro <38750803+ColegioVivasCurro@users.noreply.github.com>
2018-05-06 08:15:16 +0200

    Update Hola.java

--- Hola.java
+++ Hola.java
@@ -1,6 +1,6 @@
 public class Hola { 
 	public static void main (String args[]){ 
-		System.out.println("Welcome to de Java World"); 
+		System.out.println("Welcome to de Mundo Java"); 
         int a=0b11101; 
         System.out.println(a);
 	} 
